40fb58cc0f2d8e9d8b9bc2c234291a4d40f99e58
sakg-dev <sakg.dev.14@gmail.com>
2026-08-12 00:12:38 +0530

    update: calculator wrking and made code readable

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -2,56 +2,43 @@ use std::io;
 use std::io::Write;
 use regex::Regex;
 
-fn _print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
+fn main() {
+    loop {
+        calculate();
+    }
 }
 
-fn main() {
-    print!("Write the calculation here: ");
+fn calculate() {
+    print!("Expression: ");
     io::stdout().flush().unwrap();
     let mut input:String = String::new();
     io::stdin().read_line(&mut input).unwrap();
     input = input.trim().replace(" ", "").to_string();
-    // println!("You wrote: '{}'", input.trim());
 
-    //let parsed_input = input.split(&['+', '-', '/', '*']);
     let re = Regex::new(r"\d+|[+\-*/]").unwrap();
-    let parsed_input = re.find_iter(input.as_str());
-    
-    enum Operators {
-        None,
-        Add,
-        Subtract,
-        Multiply,
-        Divide
-    }
 
-    let mut current_operator: Operators = Operators::None;
+    let parsed_input = re.find_iter(input.as_str());
+
     let mut current_eval_val:Option<f32> = None;
 
     let mut idx = 0;
-    for part in parsed_input.into_iter(){
-        //println!("{}", part.as_str().parse::<u32>().unwrap());
-        match part.as_str().parse::<u32>() {
+    for token in parsed_input.into_iter(){ // token is of Match type
+        let token_str = token.as_str();
+        match token_str.parse::<f32>() { // to identify if its a no. or symbol, we take action on symbols
             Ok(val) => {
-                // println!("{} is an integer", val);
                 if current_eval_val == None {
-                    current_eval_val = Some(val as f32);
+                    current_eval_val = Some(val);
                 }
             },
             _ => {
-                let mut cloned_parsed_ipt = re.find_iter(input.as_str());
+                let cloned_parsed_ipt = re.find_iter(input.as_str()); // we need this bcz we need the element which is after the symbol and it consumes the iter and mess hence doing on loop
                 let Some(right) = cloned_parsed_ipt.into_iter().nth(idx+1) else {
-                    println!("!!!Something is wrong!!!");
                     return
                 };
                 let right_int = right.as_str().parse::<f32>().unwrap();
-                let symbol = part.as_str();
-
-                // println!("{} is a symbol and {:?} is on its right side", symbol, right_int);
 
                 if let Some(val) = current_eval_val {
-                    let new_eval = match symbol{
+                    let new_eval = match token_str{
                         "+" => val + right_int,
                         "-" => val - right_int,
                         "*" => val * right_int,
