816f23ce8df523fda13b4ed20b71b104222165f0
sakg-dev <sakg.dev.14@gmail.com>
2026-08-12 08:06:29 +0530

    update: calculator-used enums

--- calculator/README.md
+++ calculator/README.md
@@ -2,4 +2,11 @@ Calculator: Parse user given basic arithmetic operation and calculate
 
 e.g: f("5+7") -> 12
 
+works with max 1 expression(like 5+8 or 22/7), i would be adding for multiple too
+
 for topics: 
+    - Enums
+    - Match case and if let
+    - loops
+    - crate(yet to learn)
+    - ownership and borrowing
--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -19,30 +19,65 @@ fn calculate() {
 
     let parsed_input = re.find_iter(input.as_str());
 
-    let mut current_eval_val:Option<f32> = None;
+    enum Operator{
+        None,
+        Addition,
+        Subtraction,
+        Multiplication,
+        Divide
+    }
 
+    enum Token{
+        Integer(f32),
+        Operator(Operator)
+    }
+
+    impl Token {
+        fn new(val: &str) -> Self {
+            match val.parse::<f32>() {
+                Ok(val) => Token::Integer(val),
+                _ => Token::Operator(Operator::new(val))
+            }
+        }
+    }
+
+    impl Operator {
+        fn new(symbol: &str) -> Self {
+            match symbol {
+                "+" => Operator::Addition,
+                "-" => Operator::Subtraction,
+                "*" => Operator::Multiplication,
+                "/" => Operator::Divide,
+                _ => Operator::None
+            }
+        }
+    }
+
+    let mut current_eval_val:Option<f32> = None;
     let mut idx = 0;
-    for token in parsed_input.into_iter(){ // token is of Match type
-        let token_str = token.as_str();
-        match token_str.parse::<f32>() { // to identify if its a no. or symbol, we take action on symbols
-            Ok(val) => {
+    for match_token in parsed_input.into_iter(){ // token is of Match type
+        let token_str = match_token.as_str();
+        let token = Token::new(token_str);
+
+        match token {
+            Token::Integer(val) => {
                 if current_eval_val == None {
-                    current_eval_val = Some(val);
+                    current_eval_val = Some(val)
                 }
             },
-            _ => {
-                let cloned_parsed_ipt = re.find_iter(input.as_str()); // we need this bcz we need the element which is after the symbol and it consumes the iter and mess hence doing on loop
+            Token::Operator(op) => {
+                let cloned_parsed_ipt = re.find_iter(input.as_str());
                 let Some(right) = cloned_parsed_ipt.into_iter().nth(idx+1) else {
                     return
                 };
                 let right_int = right.as_str().parse::<f32>().unwrap();
 
-                if let Some(val) = current_eval_val {
-                    let new_eval = match token_str{
-                        "+" => val + right_int,
-                        "-" => val - right_int,
-                        "*" => val * right_int,
-                        "/" => val / right_int,
+                if let Some(val) = current_eval_val { 
+                    let new_eval = match op {
+                        Operator::Addition => val + right_int,
+                        Operator::Subtraction => val - right_int,
+                        Operator::Multiplication => val * right_int,
+                        Operator::Divide => val / right_int,
                         _ => val
                     };
                     current_eval_val = Some(new_eval);
@@ -51,6 +86,7 @@ fn calculate() {
         }
         idx += 1;
     }
+
     match current_eval_val{
         Some(evaluated_val) => println!("{} = {}", input, evaluated_val),
         None => println!("Something went wrong")
